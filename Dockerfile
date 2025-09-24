916d9565e0f436d77bc066528d63f74948f0d32a
portbound <token.api.guy@gmail.com>
2025-09-23 21:45:50 -0400

    Done: Smoke-Test

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+ARG GO_VERSION=1.25
+FROM golang:${GO_VERSION}-alpine as builder
+
+WORKDIR /app
+
+COPY go.mod .
+RUN go mod download
+
+COPY . .
+
+ARG CHALLENGE_NAME=smoke-test
+
+RUN go build -o /app/main ./cmd/${CHALLENGE_NAME}
+
+FROM alpine:latest
+
+WORKDIR /app
+
+COPY --from=builder /app/main .
+
+EXPOSE 8080
+
+ENTRYPOINT ["./main"]
---
+++ cmd/smoke-test/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go func(c net.Conn) {
+			io.Copy(c, c)
+			c.Close()
+		}(conn)
+	}
+
+}
