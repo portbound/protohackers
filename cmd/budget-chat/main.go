4161cd0df9f5fa57aa129e2684684308b250f830
portbound <token.api.guy@gmail.com>
2025-10-10 23:02:17 -0400

    Day 2: Budget Chat

--- cmd/budget-chat/main.go
+++ cmd/budget-chat/main.go
@@ -2,52 +2,99 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"net"
-	"os"
-	"slices"
+	"strings"
 	"unicode"
 )
 
-type chatRoom struct {
-	users []string
+type room struct {
+	clients        map[net.Conn]string
+	connections    chan *client
+	disconnections chan net.Conn
+	broadcasts     chan string
 }
 
-func (cr *chatRoom) Join() bool {
-	fmt.Println("Welcome to budgetchat! What shall I call you?")
-	user, err := bufio.NewReader(os.Stdin).ReadString('\n')
+type client struct {
+	conn     net.Conn
+	username string
+}
+
+func (r *room) connect(conn net.Conn) error {
+	if _, err := conn.Write([]byte("Welcome to budgetchat! What shall I call you?")); err != nil {
+		return err
+	}
+
+	username, err := bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
-		return false
+		return err
 	}
 
 	// OPTIONAL
-	if len(user) > 16 {
-		return false
+	if len(username) > 16 {
+		return errors.New("username must be less than 16 chars")
 	}
 
-	for _, char := range user {
+	for _, char := range username {
 		if !unicode.IsLetter(char) && !unicode.IsDigit(char) {
-			return false
+			return errors.New("username must be alphanumeric")
 		}
 	}
 
 	// OPTIONAL
-	if slices.Contains(cr.users, user) {
-		return false
+	if _, ok := r.clients[conn]; ok {
+		return errors.New("username must be unique")
 	}
 
-	// TODO broadcast here (?)
+	var users []string
+	for _, username := range r.clients {
+		users = append(users, username)
+	}
 
-	cr.users = append(cr.users, user)
-	return true
+	r.broadcast(fmt.Sprintf("* %s has entered the room", username))
+
+	if _, err = conn.Write([]byte("* The room contains: " + strings.Join(users, ","))); err != nil {
+		return err
+	}
+
+	r.connections <- &client{
+		conn:     conn,
+		username: username,
+	}
+
+	return nil
 }
 
-func handleConnection(conn net.Conn, room *chatRoom) {
+func (r *room) disconnect(conn net.Conn) {
+	r.disconnections <- conn
+}
+
+func (r *room) broadcast(message string) {
+}
+
+func handleRoom(room *room) {
+	select {
+	case client := <-room.connections:
+		room.clients[client.conn] = client.username
+	case client := <-room.disconnections:
+		delete(room.clients, client)
+	case <-room.broadcasts:
+	}
+}
+
+func handleConnection(conn net.Conn, room *room) {
+	defer room.disconnect(conn)
 	defer conn.Close()
 
-	if !room.Join() {
+	if err := room.connect(conn); err != nil {
 		return
+	}
+
+	for {
+		// select to send/receive from broadcasts
+		// need some way to not wait forever though, or at least accept some kind of signal that client DC'd
 	}
 }
 
@@ -59,9 +106,11 @@ func main() {
 	defer listener.Close()
 	fmt.Println("listening on port :8080")
 
-	room := chatRoom{
-		users: []string{},
+	room := room{
+		clients: make(map[net.Conn]string),
 	}
+
+	go handleRoom(&room)
 
 	for {
 		conn, err := listener.Accept()
