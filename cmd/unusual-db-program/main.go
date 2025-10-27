ab4d118bdcd76f56a85542001c6e8c29170a93df
portbound <token.api.guy@gmail.com>
2025-10-27 14:40:12 -0400

    done: Unusual DB Program

--- cmd/unusual-db-program/main.go
+++ cmd/unusual-db-program/main.go
@@ -5,10 +5,33 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"sync"
 )
 
 const port = ":8080"
+
+type request struct {
+	key       string
+	value     string
+	isWrite   bool
+	responses chan string
+}
+
+func dbManager(requests <-chan request) {
+	db := make(map[string]string)
+	db["version"] = "some stuff about the server"
+
+	for req := range requests {
+		if req.isWrite == true {
+			if _, ok := db[req.key]; ok {
+				fmt.Printf("overwriting %s with %s", req.key, req.value)
+			}
+			db[req.key] = req.value
+		} else {
+			res := db[req.key]
+			req.responses <- res
+		}
+	}
+}
 
 func main() {
 	addr, err := net.ResolveUDPAddr("udp", port)
@@ -24,44 +47,50 @@ func main() {
 
 	fmt.Printf("listening on port %s\n", port)
 
-	db := make(map[string]string)
-	db["version"] = "some stuff about the server"
-	mu := sync.Mutex{}
+	requests := make(chan request)
+	go dbManager(requests)
 
+	buf := make([]byte, 1024)
 	for {
-		buf := make([]byte, 1024)
 		n, clientAddr, err := conn.ReadFromUDP(buf)
+		// Issues related to UDP packets being dropped, delayed, or reordered are considered to be the client's problem.
+		// The server should act as if it assumes that UDP works reliably.
 		if err != nil {
 			continue
 		}
 
-		// Create a copy of the data to be used in the goroutine
+		// All requests and responses must be shorter than 1000 bytes.
+		if n > 1000 {
+			continue
+		}
+
 		data := make([]byte, n)
-		copy(data, buf[:n])
+		copy(data, buf)
+		data = bytes.TrimSpace(data)
 
-		go func() {
-			idx := bytes.Index(data, []byte{'='})
-			if idx != -1 {
-				key := string(data[:idx])
-				if key == "version" {
-					return
-				}
-				value := string(data[idx+1:])
-				mu.Lock()
-				db[key] = value
-				mu.Unlock()
-				return
+		var req request
+		index := bytes.Index(data, []byte{'='})
+		if index != -1 {
+			req.isWrite = true
+			req.key = string(data[:index])
+			req.value = string(data[index+1:])
+			if req.key != "version" {
+				requests <- req
 			}
+			continue
+		}
 
-			key := string(data)
-			mu.Lock()
-			value := db[key]
-			mu.Unlock()
+		req.isWrite = false
+		req.key = string(data)
+		req.responses = make(chan string)
+		requests <- req
 
-			_, err := conn.WriteToUDP([]byte(value), clientAddr)
-			if err != nil {
-				// do nothing
+		go func(ca *net.UDPAddr) {
+			value := <-req.responses
+			response := fmt.Sprintf("%s=%s", req.key, value)
+			if _, err = conn.WriteToUDP([]byte(response), clientAddr); err != nil {
+				log.Printf("DEBUG: WriteToUDP error: %v", err)
 			}
-		}()
+		}(clientAddr)
 	}
 }
