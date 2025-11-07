10ca5c18c4b6c920384f577d43952e68e9e451b9
portbound <token.api.guy@gmail.com>
2025-11-06 23:00:19 -0500

    speed daemon handling client lifecycle

--- cmd/speed-daemon/main.go
+++ cmd/speed-daemon/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"io"
 	"log"
 	"net"
 )
@@ -9,6 +13,7 @@ const port = ":8080"
 
 // DATA TYPE
 // type str is a length-prefixed byte slice
+// 0-255 length ASCII char codes
 type str []byte
 
 // MESSAGES
@@ -16,6 +21,9 @@ type str []byte
 type Error struct {
 	msg str
 }
+
+func NewError(conn net.Conn) {}
+
 type Plate struct {
 	plate     str
 	timestamp uint32
@@ -33,28 +41,49 @@ type WantHeartbeat struct {
 	interval uint32
 }
 type Heartbeat struct{}
+
+// cameras report each plate they observe, along with a timestamp
+// timestamps are unix timestamps that are unisgned
 type IAmCamera struct {
 	road  uint16
 	mile  uint16
 	limit uint16
 }
+
+// when server detects car speeding at 2 points on the same road, it finds the corresponding dispatcher and has it deliver a ticket
 type IAmDispatcher struct {
 	numroads uint8
 	roads    []uint16
 }
 
-// each camera provides is fields when it connects to the server
-// cameras report each plate they observe, along with a timestamp
-// timestamps are unix timestamps that are unisgned
-type camera struct {
-	road       uint16
-	location   string
-	speedLimit int
+func handleCamera(conn net.Conn, signal chan struct{}) {
+	defer func() {
+		signal <- struct{}{}
+	}()
+
+	buf := make([]byte, 6)
+	_, err := io.ReadFull(conn, buf)
+	if err != nil {
+		log.Printf("failed to read camera metadata from conn: %w", err)
+		return
+	}
+
+	c := IAmCamera{
+		road:  binary.BigEndian.Uint16(buf[0:2]),
+		mile:  binary.BigEndian.Uint16(buf[2:4]),
+		limit: binary.BigEndian.Uint16(buf[4:6]),
+	}
+
+	// add camera to a map of cameras via channel
+	for {
+		// Go do camera things
+	}
 }
 
-// when server detects car speeding at 2 points on the same road, it finds the corresponding dispatcher and has it deliver a ticket
-type dispatcher struct {
-	roads []uint16
+func handlerDispatcher(conn net.Conn, signal chan struct{}) {
+	defer func() {
+		signal <- struct{}{}
+	}()
 }
 
 // roads are identified as 0 - 65535
@@ -63,6 +92,25 @@ type dispatcher struct {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
+
+	r := bufio.NewReader(conn)
+	b, err := r.ReadByte()
+	if err != nil {
+		log.Printf("failed to read msgType: %s", err)
+		return
+	}
+
+	signal := make(chan struct{})
+	switch b {
+	case 0x80:
+		go handleCamera(conn, signal)
+		<-signal
+	case 0x81:
+		go handlerDispatcher(conn, signal)
+		<-signal
+	default:
+		log.Printf("recieved invalid clientType: 0x%x", b)
+	}
 }
 
 func main() {
