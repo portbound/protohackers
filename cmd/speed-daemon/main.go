aa65cd7c67989514c8787cdb330d1ad9fac7d658
portbound <token.api.guy@gmail.com>
2025-11-08 15:13:04 -0500

    refactoring speed daemon

--- cmd/speed-daemon/main.go
+++ cmd/speed-daemon/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/binary"
+	"fmt"
 	"io"
 	"log"
 	"net"
@@ -17,20 +18,31 @@ const port = ":8080"
 // DATA TYPE
 // type str is a length-prefixed byte slice
 // 0-255 length ASCII char codes
-type str []byte
+type str struct {
+	len  uint8
+	body []byte
+}
 
 // MESSAGES
+type message interface {
+	isMessage()
+}
+
 // each message begins with a sing u8
-type Error struct {
+type Error struct { // 0x10
 	msg str
 }
 
-type Plate struct {
+func (e *Error) isMessage()
+
+type Plate struct { // 0x20
 	plate     str
 	timestamp uint32
 }
 
-type Ticket struct {
+func (p *Plate) isMessage()
+
+type Ticket struct { // 0x21
 	plate      str
 	road       uint16
 	mile1      uint16
@@ -39,71 +51,133 @@ type Ticket struct {
 	timestamp2 uint32
 	speed      uint16 // 100x mph
 }
-type WantHeartbeat struct {
+
+func (t *Ticket) isMessage()
+
+type WantHeartbeat struct { // 0x40
 	interval uint32
 }
-type Heartbeat struct{}
+
+func (wh *WantHeartbeat) isMessage()
+
+type Heartbeat struct{} // 0x41
+func (h *Heartbeat) isMessage()
 
 // cameras report each plate they observe, along with a timestamp
 // timestamps are unix timestamps that are unisgned
-type IAmCamera struct {
+type IAmCamera struct { // 0x80
 	road  uint16
 	mile  uint16
 	limit uint16
 }
 
+func (c *IAmCamera) isMessage()
+
 // when server detects car speeding at 2 points on the same road, it finds the corresponding dispatcher and has it deliver a ticket
-type IAmDispatcher struct {
+type IAmDispatcher struct { // 0x81
 	numroads uint8
 	roads    []uint16
 }
 
-type Camera struct {
+func (d *IAmDispatcher) isMessage()
+
+type ClientDisconnect struct {
 	conn net.Conn
-	data IAmCamera
+}
+
+func (c *ClientDisconnect) isMessage()
+
+type Camera struct {
+	conn     net.Conn
+	data     IAmCamera
+	msgTypes map[uint8]struct{}
 }
 
 type Dispatcher struct {
-	conn net.Conn
-	data IAmDispatcher
+	conn     net.Conn
+	data     IAmDispatcher
+	msgTypes map[uint8]struct{}
 }
 
-func handleCamera(conn net.Conn, newCameras chan Camera) {
+func newCameraFromConn(conn net.Conn) (*Camera, error) {
 	buf := make([]byte, 6)
 	_, err := io.ReadFull(conn, buf)
 	if err != nil {
-		log.Printf("failed to read camera metadata from conn: %v", err)
-		return
+		return nil, fmt.Errorf("failed to read camera metadata from conn: %v", err)
 	}
 
-	newCameras <- Camera{
+	return &Camera{
 		conn: conn,
 		data: IAmCamera{
 			road:  binary.BigEndian.Uint16(buf[0:2]),
 			mile:  binary.BigEndian.Uint16(buf[2:4]),
 			limit: binary.BigEndian.Uint16(buf[4:6]),
 		},
-	}
+	}, nil
+}
 
+func handleCamera(c *Camera, cameraEvents chan message) {
+	defer func() {
+		cameraEvents <- &ClientDisconnect{conn: c.conn}
+	}()
+
+	r := bufio.NewReader(c.conn)
 	for {
-		// Go do camera things
+		b, err := r.ReadByte()
+		if err != nil {
+			log.Printf("connection to camera was lost: %v", err)
+			return
+		}
+
+		if b == 0x20 {
+			var p Plate
+			// read first byte to get len of str
+			// read len of str into str
+			// read remaining uint32 (unix timestamp) into timestamp
+			// send to update channel a message
+			cameraEvents <- &p
+			continue
+		}
+
+		if b == 0x40 {
+			cameraEvents <- &WantHeartbeat{}
+			continue
+		}
+
+		// b is not a valid message type, handle error
 	}
 }
 
-func handleDispatcher(conn net.Conn, newDispatchers chan Dispatcher) {}
-
-func stateMachine(newCamera chan Camera, newDispatcher chan Dispatcher) {
+func cameraManager(newCamera <-chan *Camera, cameraEvents <-chan message) {
 	cameras := make(map[net.Conn]IAmCamera)
-	dispatchers := make(map[net.Conn]IAmDispatcher)
 
 	for {
 		select {
 		case c := <-newCamera:
 			cameras[c.conn] = c.data
-		case d := <-newDispatcher:
-			dispatchers[d.conn] = d.data
+		case event := <-cameraEvents:
+			switch e := event.(type) {
+			case *Plate:
+			// register plate and check to see if we have a potential ticket
+			case *WantHeartbeat:
+			// send heartbeat
+			case *ClientDisconnect:
+				delete(cameras, e.conn)
+			}
 		}
 	}
+}
+
+func newDispatcherFromConn(c net.Conn) (*Dispatcher, error) {
+	panic("unimplemented")
+}
+
+func handleDispatcher(dispatcher *Dispatcher, dispatcherEvents chan message) {
+	panic("unimplemented")
+}
+
+func dispatcherManager(newDispatchers chan *Dispatcher, dispatcherEvents chan message) {
+	panic("unimplemented")
 }
 
 func main() {
@@ -113,9 +187,13 @@ func main() {
 	}
 	defer listener.Close()
 
-	newCameras := make(chan Camera)
-	newDispatchers := make(chan Dispatcher)
-	go stateMachine(newCameras, newDispatchers)
+	newCameras := make(chan *Camera)
+	cameraEvents := make(chan message)
+	go cameraManager(newCameras, cameraEvents)
+
+	newDispatchers := make(chan *Dispatcher)
+	dispatcherEvents := make(chan message)
+	go dispatcherManager(newDispatchers, dispatcherEvents)
 
 	for {
 		conn, err := listener.Accept()
@@ -134,11 +212,23 @@ func main() {
 
 			switch b {
 			case 0x80:
-				handleCamera(c, newCameras)
+				camera, err := newCameraFromConn(c)
+				if err != nil {
+					log.Printf("failed to setup Camera for %v: %v", c, err)
+					return
+				}
+				newCameras <- camera
+				handleCamera(camera, cameraEvents)
 			case 0x81:
-				handleDispatcher(c, newDispatchers)
+				dispatcher, err := newDispatcherFromConn(c)
+				if err != nil {
+					log.Printf("failed to setup Dispatcher for %v: %v", c, err)
+					return
+				}
+				newDispatchers <- dispatcher
+				handleDispatcher(dispatcher, dispatcherEvents)
 			default:
-				log.Printf("recieved invalid clientType: 0x%x", b)
+				log.Printf("failed to set up client, msg type 0x%x is invalid", b)
 			}
 		}(conn)
 	}
