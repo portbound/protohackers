401d1cf6b92d60310be3afc4afd76eb9326de90a
portbound <token.api.guy@gmail.com>
2025-11-12 22:41:58 -0500

    wip

--- cmd/speed-daemon/camera.go
+++ cmd/speed-daemon/camera.go
@@ -5,10 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sort"
 	"time"
 )
 
 var cameras map[net.Conn]*IAmCamera
+
+type cust struct {
+	*IAmCamera
+	*Plate
+}
 
 func newCameraFromConn(conn net.Conn, cameraEvents chan<- *event) error {
 	var m IAmCamera
@@ -56,14 +62,15 @@ func handleCamera(conn net.Conn, cameraEvents chan<- *event) {
 }
 
 func cameraManager(cameraEvents chan *event) {
-	roads := make(map[uint16][]*Plate)
+
+	roads := make(map[uint16][]*cust)
 	heartbeats := make(map[net.Conn]uint32)
 
 	// not sure if we need to close this channel anywhere? since it's technically supposed to run until the end of the program, it might be fine, but keep in mind this blocks forever
 	for e := range cameraEvents {
-		switch t := e.msg.(type) {
+		switch msg := e.msg.(type) {
 		case *IAmCamera:
-			cameras[e.conn] = t
+			cameras[e.conn] = msg
 		case *Plate:
 			camera, ok := cameras[e.conn]
 			if !ok {
@@ -71,13 +78,26 @@ func cameraManager(cameraEvents chan *event) {
 				continue
 			}
 
-			road, ok := roads[camera.road]
-			if !ok {
-				roads[camera.road] = append(roads[camera.road], t)
-				continue
+			// not sure if we need this check, it makes sense, we don't need to perform a ticket check if this is the first instance of a plate on that particular road, but I don't know if it really matters, and this is hard to understand at a glance. It's probably better to just "check" since it should be super fast with a single plate anyway
+
+			// road, ok := roads[camera.road]
+			// if !ok {
+			// 	// roads[camera.road] = append(roads[camera.road], msg)
+			// 	roads[camera.road] = append(roads[camera.road], &cust{
+			// 		IAmCamera: camera,
+			// 		Plate:     msg,
+			// 	})
+			// 	continue
+			// }
+
+			c := cust{
+				IAmCamera: camera,
+				Plate:     msg,
 			}
 
-			performTicketCheck(road, t)
+			roads[camera.road] = append(roads[camera.road], &c)
+
+			performTicketCheck(roads[camera.road], &c)
 		case *WantHeartbeat:
 			if _, ok := heartbeats[e.conn]; ok {
 				sendError(e.conn, fmt.Sprintf("Client: %v\nError: it is an error for a client to send multiple WantHeartbeat messages on a single connection", e.conn))
@@ -85,8 +105,8 @@ func cameraManager(cameraEvents chan *event) {
 				continue
 			}
 
-			heartbeats[e.conn] = t.interval
-			go handleHeartbeat(e.conn, t.interval)
+			heartbeats[e.conn] = msg.interval
+			go handleHeartbeat(e.conn, msg.interval)
 		case *ClientDisconnect:
 			delete(cameras, e.conn)
 		}
@@ -94,9 +114,9 @@ func cameraManager(cameraEvents chan *event) {
 }
 
 func handleHeartbeat(conn net.Conn, interval uint32) {
+	var h Heartbeat
 	for {
 		time.Sleep(time.Duration(interval/10) * time.Second)
-		var h Heartbeat
 		if err := h.encode(conn); err != nil {
 			log.Printf("failed to send heartbeat to client %v: %e", conn, err)
 			return
@@ -104,10 +124,27 @@ func handleHeartbeat(conn net.Conn, interval uint32) {
 	}
 }
 
-func performTicketCheck([]*Plate, *Plate) {
-	// if we have a current plate check for say 'mile 9' we're only interested in matching plates for 'mile 8' (I think?)
-	// there's probably an algorithm we can use to streamline this, but i'd imagine the o(n) solution is probably fine for this challenge
-	// We need to touch every plate to see if it's road number is our plate - 1 and then see if the plate actually matches
-	// We do need to touch every plate though because the instructions say that clients may not send their plates on time, so the plates arent' actually sorted or anything
-	// So yeah, I guess simple sort is probably pretty straightforward here
+func performTicketCheck(road []*cust, c *cust) {
+	sort.Slice(road, func(i int, j int) bool {
+		return road[i].mile < road[j].mile
+	})
+
+	// Must be sorted!!!
+
+	// cameras may glitch and fail to take the picture at a given mile mark
+	// plate pics may come out of order
+	// can't send tickets twice
+
+	// with these constraints the solution seems to be as follows:
+	// By sorting the set, we can basically just find the index of the passed in cust and then search left until we find the first occurence of that plate at a lower mile mark, and then search right until we find the first occurence at a higher mile mark.
+	// Only write tickets that contain the passed in cust. Since this is new, there will never be a dupe.
+	// the only place this falls apart I think is if we receive images for a car speeding at mile 4 and then 6. We write a ticket and move on. Then later on a picture for mile 5 comes in and we do our check which should now technically return 2 more tickets.
+	// So in total, the car gets tickets for 4>5, 5>6, and also 4>6
+	// this does not seem right
+
+	// var speed int
+	// for i := len(road) - 1; i >= 0; i-- {
+	// 	if bytes.Equal(road[i].plate.body, plate.plate.body) {
+	// }
+	// }
 }
