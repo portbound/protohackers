51827b4601c5b3f2926c7ce3c1f89d9add3ce81a
portbound <token.api.guy@gmail.com>
2025-11-20 22:29:35 -0500

    random cleanup

--- cmd/speed-daemon/main.go
+++ cmd/speed-daemon/main.go
@@ -16,7 +16,7 @@ type event struct {
 
 func sendError(conn net.Conn, msg string) {
 	var e Error
-	e.msg.body = fmt.Appendf(e.msg.body, msg)
+	e.msg.body = fmt.Append(e.msg.body, msg)
 	err := e.encode(conn)
 	if err != nil {
 		log.Printf("failed to send Error to client %v: %v", conn, err)
