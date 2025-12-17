6500fed032d411471be0adc20bf6cef908ee304a
portbound <token.api.guy@gmail.com>
2025-12-16 22:31:12 -0500

    working(?) sort of... Ticket check for single car and 2 cars seems to work if checking right only

--- cmd/speed-daemon/main.go
+++ cmd/speed-daemon/main.go
@@ -41,7 +41,6 @@ func sendErrorAndDisconnect(conn net.Conn, msg string) {
 }
 
 func handleHeartbeat(conn net.Conn, msg *WantHeartbeat, signal chan struct{}) {
-	fmt.Printf("client %v: hearbeat '%d'\n", conn, msg.Interval)
 	if msg.Interval == 0 {
 		return
 	}
@@ -54,12 +53,10 @@ func handleHeartbeat(conn net.Conn, msg *WantHeartbeat, signal chan struct{}) {
 			break
 		}
 	}
-	fmt.Printf("client %v: done\n", conn)
 	signal <- struct{}{}
 }
 
-func ticketCheck(s *Sighting, plt string, sightings []*Sighting, tickets map[string]map[uint32]struct{}, camera *IAmCamera) {
-	fmt.Printf("len sightings: %d", len(sightings))
+func ticketCheck(s *Sighting, plt string, sightings []*Sighting, tickets map[string]map[uint32]struct{}, camera *IAmCamera) *Ticket {
 	slices.SortFunc(sightings, func(a, b *Sighting) int {
 		if a.Timestamp < b.Timestamp {
 			return -1
@@ -70,97 +67,108 @@ func ticketCheck(s *Sighting, plt string, sightings []*Sighting, tickets map[str
 		return 0
 	})
 	idx := slices.Index(sightings, s)
-	fmt.Printf("idx: %v\n", idx)
 
-	fmt.Println("begin ticket check")
-	tix := tickets[plt]
-	fmt.Printf("day: %d\n", s.Timestamp/86400)
+	tix, ok := tickets[plt]
+	if !ok {
+		tix = make(map[uint32]struct{})
+		tickets[plt] = tix
+	}
 	if _, ok := tix[s.Timestamp/86400]; ok { // TODO need to check this logic
-		return // we have a ticket for this day already and should continue
+		return nil // we have a ticket for this day already and should continue
 	}
 
 	curr := sightings[idx]
 
+	var t *Ticket
 	if idx > 0 {
 		fmt.Println("checking left")
 		left := sightings[idx-1]
 		distance := curr.Mile - left.Mile
 		time := curr.Timestamp - left.Timestamp
 		speed := (float64(distance) / float64(time)) * 3600.0
-
 		if speed > float64(camera.Limit) {
-			t := &Ticket{
+			t = &Ticket{
 				Mile1:      left.Mile,
 				Timestamp1: left.Timestamp,
 				Mile2:      curr.Mile,
 				Timestamp2: curr.Timestamp,
 				Speed:      uint16(speed) * 100,
 			}
-			fmt.Printf("ticket: %+v\n", t)
 			tix[t.Timestamp1/86400] = struct{}{}
 			tix[t.Timestamp2/86400] = struct{}{}
-			return
+			return t
 		}
 	}
 
 	if idx < len(sightings) {
 		fmt.Println("checking right")
 		right := sightings[idx+1]
-		distance := curr.Mile - right.Mile
-		time := curr.Timestamp - right.Timestamp
-		speed := (float64(distance) / float64(time)) * 3600.0 // TODO need to check speed calc 
-
+		distance := right.Mile - curr.Mile
+		time := right.Timestamp - curr.Timestamp
+		speed := (float64(distance) / float64(time)) * 3600.0 // TODO need to check speed calc
 		if speed > float64(camera.Limit) {
-			t := &Ticket{
-				Mile1:      right.Mile,
-				Timestamp1: right.Timestamp,
-				Mile2:      curr.Mile,
-				Timestamp2: curr.Timestamp,
+			t = &Ticket{
+				Mile1:      curr.Mile,
+				Timestamp1: curr.Timestamp,
+				Mile2:      right.Mile,
+				Timestamp2: right.Timestamp,
 				Speed:      uint16(speed) * 100,
 			}
-			fmt.Printf("ticket: %+v\n", t)
 			tix[t.Timestamp1/86400] = struct{}{}
 			tix[t.Timestamp2/86400] = struct{}{}
+			return t
 		}
 	}
 
+	return nil
 }
 func serverManager(events chan *Event) {
 	cameras := make(map[net.Conn]*IAmCamera)
 	dispatchers := make(map[net.Conn]*IAmDispatcher)
-	var tickets map[string]map[uint32]struct{} // map[plate]map[day]struct{}
+	var tickets = make(map[string]map[uint32]struct{}) // map[plate]map[day]struct{}
 	var heartbeats = make(map[net.Conn]*WantHeartbeat)
 	var roads = make(map[uint16]map[string][]*Sighting)
 
 	for e := range events {
 		switch msg := e.Msg.(type) {
 		case *Plate:
-			fmt.Printf("plate: %+v\n", msg)
 			if camera, ok := cameras[e.Conn]; ok {
 				road, ok := roads[camera.Road]
 				if !ok {
-					fmt.Printf("creating road for %d\n", camera.Road)
 					roads[camera.Road] = make(map[string][]*Sighting)
 					road = roads[camera.Road]
-				} else {
-					fmt.Printf("road exists for %d\n", camera.Road)
 				}
 
 				s := Sighting{
 					Mile:      camera.Mile,
 					Timestamp: msg.Timestamp,
 				}
-				fmt.Printf("sighting: %+v\n", s)
-				plt := string(msg.Plate.Body)
-				road[plt] = append(road[plt], &s)
-				fmt.Printf("appended sightings: %+v\n", road[plt])
+				// plate := str{
+				// 	Len:  msg.Plate.Len,
+				// 	Body: msg.Plate.Body,
+				// }
+				p := string(msg.Plate.Body)
+				road[p] = append(road[p], &s)
 
-				fmt.Println(len(road[plt]))
-
-				if len(road[plt]) == 1 {
+				if len(road[p]) == 1 {
 					continue
 				}
-				ticketCheck(&s, plt, road[plt], tickets, camera)
+
+				ticket := ticketCheck(&s, p, road[p], tickets, camera)
+				ticket.Plate = str{
+					Len:  msg.Plate.Len,
+					Body: msg.Plate.Body,
+				}
+				ticket.Road = camera.Road
+				fmt.Printf("%+v\n", ticket)
+				for conn, dispatcher := range dispatchers {
+					if slices.Contains(dispatcher.Roads, camera.Road) {
+						if err := ticket.encode(conn); err != nil {
+							log.Fatalf("failed to send ticket to client %v: %v", conn, err)
+						}
+						break
+					}
+				}
 			} else {
 				sendErrorAndDisconnect(e.Conn, fmt.Sprintf("Client: %v\nError: It is an error for a client that has not identified itself as a camera to send a Plate message.", e.Conn))
 			}
@@ -221,9 +229,7 @@ func handleConnection(conn net.Conn, events chan *Event) {
 			}
 			events <- &e
 			<-e.Signal
-			fmt.Println("plate done???")
 		case byte(MsgWantHeartBeat):
-			fmt.Printf("new heartbeat client: %v\n", conn)
 			var m WantHeartbeat
 			if err := m.decode(conn); err != nil {
 				log.Printf("heartbeat setup failed for client %v: %v", conn, err)
@@ -237,9 +243,7 @@ func handleConnection(conn net.Conn, events chan *Event) {
 			}
 			events <- &e
 			<-e.Signal
-			fmt.Println("new heartbeat client SETUP COMPLETE")
 		case byte(MsgIAmCamera):
-			fmt.Printf("new camera client: %v\n", conn)
 			var m IAmCamera
 			if err := m.decode(conn); err != nil {
 				log.Printf("camera setup failed for client %v: %v\n", conn, err)
@@ -253,9 +257,7 @@ func handleConnection(conn net.Conn, events chan *Event) {
 			}
 			events <- &e
 			<-e.Signal
-			fmt.Println("new camera client SETUP COMPLETE")
 		case byte(MsgIAmDispatcher):
-			fmt.Printf("new dispatcher client: %v\n", conn)
 			var m IAmDispatcher
 			if err := m.decode(conn); err != nil {
 				log.Printf("dipatcher setup failed for client %v: %v\n", conn, err)
@@ -269,7 +271,6 @@ func handleConnection(conn net.Conn, events chan *Event) {
 			}
 			events <- &e
 			<-e.Signal
-			fmt.Println("new dispatcher client SETUP COMPLETE")
 		default:
 			sendErrorAndDisconnect(conn, fmt.Sprintf("Error: It is an error for a client to send the server a message with any message type value that is not listed below with 'Client->Server': 0x%x.\n", b))
 		}
